7fd394201cec62ab458c771ed24dcbdd5ea911ca
Bastian B <bastian.brito.g@usach.cl>
2024-12-08 00:07:50 -0300

    No funciona gateway

--- gateway-service/src/main/java/gateway_service/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/gateway_service/gateway_service/GatewayServiceApplication.java
@@ -3,6 +3,8 @@ package gateway_service.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 
 public class GatewayServiceApplication {
