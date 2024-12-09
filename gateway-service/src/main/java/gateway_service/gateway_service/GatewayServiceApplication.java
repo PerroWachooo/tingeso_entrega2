3b0554bcc79c273fdc49fb9b1c1e114278d3e019
Bastian B <bastian.brito.g@usach.cl>
2024-12-09 00:27:45 -0300

    Se agrega todo lo que es deployment, faltan los deployment de las bd

--- gateway-service/src/main/java/gateway_service/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/gateway_service/gateway_service/GatewayServiceApplication.java
@@ -3,8 +3,6 @@ package gateway_service.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 
 public class GatewayServiceApplication {
